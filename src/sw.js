ca0cc661fc9695e231f9e39ffae95f291e762f26
Aditya Agarwal <aditya81070@gmail.com>
2018-09-27 12:31:11 +0530

    Add: cache on install event
    This open the cache if there is no any cache opened yet and add paths of file to cache

---
+++ src/sw.js
@@ -0,0 +1,48 @@
+const staticCacheName = 'mws-restaurant-v1'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(staticCacheName)
+    .then((cache) => {
+      return cache.addAll(['./',
+        './js/main.js', './js/dbhelper.js', './js/ServiceWorkerController.js', './js/restaurant_info.js',
+        './img/1.jpg', './img/2.jpg', './img/3.jpg', './img/4.jpg', './img/5.jpg', './img/6.jpg', './img/7.jpg', './img/8.jpg', './img/9.jpg', './img/10.jpg',
+        './data/restaurants.json',
+        './css/styles.css', './css/bootstrap.min.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+      ])
+    }))
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request)
+    .then((response) => {
+      return response ||
+      caches.open(staticCacheName)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 404) {
+                return new Response('page not found')
+              }
+              if (event.request.url.indexOf('restaurant.html') !== -1 || event.request.url.indexOf('leaflet') !== -1) {
+                cache.put(event.request, response.clone())
+              }
+              return response
+            })
+        })
+    })
+    .catch(() => {
+      return new Response('You seems completely offline')
+    }))
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys()
+    .then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter( cacheName => cacheName.startsWith('mws-') && cacheName !== staticCacheName)
+          .map(cacheName => caches.delete(cacheName))
+      )
+    }))
+})
