7a0b209215123bf18d72ab36c7c6739fc1b95e88
Aditya Agarwal <aditya81070@gmail.com>
2018-09-27 14:08:54 +0530

    Add notify on update in service worker
     This notify the user about new update in service worker. In this there are certain condition in which we have to tell user about new update

--- src/js/ServiceWorkerController.js
+++ src/js/ServiceWorkerController.js
@@ -16,5 +16,35 @@ ServiceWorkerController.prototype._registerServiceWorker = function () {
   navigator.serviceWorker.register('./sw.js')
     .then((reg) => {
       console.log(`Service worker has been registered with scope: ${reg.scope}`)
+      if (!navigator.serviceWorker.controller) {
+        return
+      }
+      if (reg.waiting) {
+        serviceWorkerController._updateReady()
+        return
+      }
+      if (reg.installing) {
+        serviceWorkerController._trackInstalling()
+        return
+      }
+      reg.addEventListener('updatefound', () => {
+        serviceWorkerController._trackInstalling(reg.installing)
+      })
     })
+    .catch((err) => {
+      console.log(`Service Worker registration failed ${err}`)
+    })
+}
+
+ServiceWorkerController.prototype._trackInstalling = function _trackInstalling (worker) {
+  let ServiceWorkerController = this
+  worker.addEventListener('statechange', () => {
+    if (worker.state === 'installed') {
+      ServiceWorkerController._updateReady()
+    }
+  })
+}
+
+ServiceWorkerController.prototype._updateReady = function _updateReady () {
+  alert('there is a new update')
 }
