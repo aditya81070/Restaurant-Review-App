828683fa6a55c39345d0b72cc10af36cec395d4f
Aditya Agarwal <aditya81070@gmail.com>
2018-09-28 01:38:49 +0530

    sw.js: Add documentation

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,9 @@
+// name of static cache
 const staticCacheName = 'mws-restaurant-v1'
 
+/**
+ * Install service worker and cache all pages and assets required for offline access
+ */
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(staticCacheName)
     .then((cache) => {
@@ -14,29 +18,9 @@ self.addEventListener('install', (event) => {
     }))
 })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request)
-    .then((response) => {
-      return response ||
-        caches.open(staticCacheName)
-          .then((cache) => {
-            return fetch(event.request)
-              .then((response) => {
-                if (response.status === 404) {
-                  return new Response('page not found')
-                }
-                if (event.request.url.indexOf('restaurant.html') !== -1 || event.request.url.indexOf('leaflet') !== -1) {
-                  cache.put(event.request, response.clone())
-                }
-                return response
-              })
-          })
-    })
-    .catch(() => {
-      return new Response('You seems completely offline')
-    }))
-})
-
+/**
+ * Activate service worker and delete old cache (if any) before add new cache
+ */
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
@@ -51,6 +35,46 @@ self.addEventListener('activate', (event) => {
         )
       }))
 })
+
+/**
+ * Intercept all requests and return response from cache if matched againse cache
+ */
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request)
+    .then((response) => {
+      /**
+       * If we have matching response, we return the cache value, otherwise we return
+       * the result of call to fetch(), which will make a network request and return the response
+       * if anything is retrieved from network
+       */
+      return response ||
+        caches.open(staticCacheName)
+          .then((cache) => {
+            return fetch(event.request)
+              .then((response) => {
+                if (response.status === 404) {
+                  return new Response('page not found')
+                }
+                /**
+                 * If we want to cache new requests cumulatively , we do so by handling response
+                 * of fetch request and then adding it to the cache.
+                 */
+                if (event.request.url.indexOf('restaurant.html') !== -1 || event.request.url.indexOf('leaflet') !== -1) {
+                  cache.put(event.request, response.clone())
+                }
+                return response
+              })
+          })
+    })
+    .catch(() => {
+      // if both (cache and n/w) fails, show a generic message
+      return new Response('You seems completely offline, and we did not find any cache for URL')
+    }))
+})
+/**
+ * listen for the 'message' event, and call
+ * 'skipWaiting' if user want to update the page and get the appropriate message
+ */
 self.addEventListener('message', (event) => {
   if (event.data === 'SWupdate') {
     self.skipWaiting()
