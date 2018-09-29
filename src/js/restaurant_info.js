9fb5e8a8770e399ba60c60239a48021de943f8fd
Aditya Agarwal <aditya81070@gmail.com>
2018-09-29 13:22:17 +0530

    Add aria-current to the last breadcrumb link

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -168,7 +168,12 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb')
   const li = document.createElement('li')
-  li.innerHTML = restaurant.name
+  const a = document.createElement('a')
+  a.href = window.location
+  // add screen reader accessibility to breadcrumb link
+  a.setAttribute('aria-current', 'page')
+  a.innerHTML = restaurant.name
+  li.appendChild(a)
   breadcrumb.appendChild(li)
 }
 
